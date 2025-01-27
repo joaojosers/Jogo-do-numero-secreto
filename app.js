e8af5a6c796dad0c3aab102f8aff0afc7a950fe0
Joao Jose Rocha de Sousa <joaojoserodeso@gmail.com>
2025-01-27 18:05:16 -0300

    alterar para gerar numero aleatorio até 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -25,7 +25,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
     // responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
